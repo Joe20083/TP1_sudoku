914007746c176a1720b213d34c2ce3bee34175c4
Joe20083 <117241070+Joe20083@users.noreply.github.com>
2024-10-31 14:27:38 -0400

    Add files via upload

---
+++ IntegerBoard.java
@@ -0,0 +1,48 @@
+
+
+public class IntegerBoard<T> implements GameBoard<T>{
+
+    private final int width ;
+    private final int height;
+    private final T[][] board;
+
+    // Constructor to initialize the board with given width and height
+    public IntegerBoard(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.board = (T[][]) new Object[width][height];  // Create a 2D array for the board
+    }
+
+
+    @Override
+    public T getCell(int x, int y) {
+        if (isValidCell(x, y)) {
+            return board[x][y];
+        } else {
+            throw new IndexOutOfBoundsException("Invalid cell coordinates");
+        }
+    }
+
+    @Override
+    public void setCell( int x, int y, T value ) {
+        if (isValidCell(x, y)) {
+            board[x][y] = value;
+        } else {
+            throw new IndexOutOfBoundsException("Invalid cell coordinates");
+        }
+    }
+
+
+    public boolean isValidCell(int x, int y) {
+        return x >= 0 && x < width && y >= 0 && y < height;
+    }
+
+    int getWidth();
+
+
+    int getHeight();
+
+
+    void display();
+
+}
